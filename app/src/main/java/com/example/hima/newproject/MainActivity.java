e0b5743d437a16136c9437f15f67075b1a2bef92
hima <hima@peppersquare.com>
2016-03-11 16:43:29 +0530

    Added OnClickListener

--- app/src/main/java/com/example/hima/newproject/MainActivity.java
+++ app/src/main/java/com/example/hima/newproject/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.hima.newproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText mUsername;
     private EditText mPassword;
     private Button mButton;
@@ -18,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
         mUsername = (EditText) findViewById(R.id.username);
         mPassword = (EditText) findViewById(R.id.password);
         mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
 
+                break;
+        }
     }
 }
