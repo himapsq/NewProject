c5ba1910838a53e622fb774704bfa79a25011727
hima <hima@peppersquare.com>
2016-03-11 16:36:13 +0530

    Added resource

--- app/src/main/java/com/example/hima/newproject/MainActivity.java
+++ app/src/main/java/com/example/hima/newproject/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.hima.newproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mUsername;
+    private EditText mPassword;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mUsername = (EditText) findViewById(R.id.username);
+        mPassword = (EditText) findViewById(R.id.password);
+        mButton = (Button) findViewById(R.id.button);
+
     }
 }
