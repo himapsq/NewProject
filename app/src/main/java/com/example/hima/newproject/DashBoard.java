7d3a7ff992c891d903aa2b2b5e8b494313c7f3a6
saikiranpsq <saikiran@peppersquare.com>
2016-03-14 12:05:46 +0530

    removed tool bar in dashboard

--- app/src/main/java/com/example/hima/newproject/DashBoard.java
+++ app/src/main/java/com/example/hima/newproject/DashBoard.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,8 +13,6 @@ public class DashBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         Bundle bundle = getIntent().getExtras();
         String user = bundle.getString("username");
